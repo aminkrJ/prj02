5004565f5446046b07b1245bbf1b60d264e7c6a3
Amin Karaji <amin.krj@gmail.com>
2017-07-12 12:50:11 +1000

    Adding generic reducer for errors

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { routerReducer } from 'react-router-redux';
 
 import cart from './cartReducer'
 import products from './productsReducer'
+import errors from './errorsReducer'
 
 export default combineReducers({
   routing: routerReducer,
   cart,
+  errors,
   products
 })
