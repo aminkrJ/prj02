8fa7747be17565a6fb2afab5bd4d3f73868f9ff8
Amin Karaji <amin.karaji@siteminder.com>
2017-07-19 14:17:38 +1000

    removing unnecessary include

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { beginTask, endTask } from 'redux-nprogress';
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
