46f1408c25cd56ec987d8ea549e83f10280160d2
Amin Karaji <amin.krj@gmail.com>
2017-09-13 21:36:09 +1000

    Make hero and featured products work

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import $ from 'jquery/src/jquery';
 import 'owl.carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
@@ -11,12 +12,12 @@ class Carousel extends Component {
   }
 
   componentDidMount() {
-    $('.owl-carousel').owlCarousel(this.props)
+    $("#" + this.props.id).owlCarousel(this.props)
   }
 
   render() {
     return (
-      <div className="owl-carousel large-controls dots-inside">
+      <div id={this.props.id} className={ classNames("owl-carousel", {"large-controls": this.props.largeControls, "dots-inside": this.props.dotsInside}) }>
         {this.props.children}
       </div>
     )
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -19,9 +19,9 @@ class Home extends Component {
     this.props.addToCart(product)
   }
 
-  renderProductsSlider() {
-    return(
-      this.props.products.entities.map((product, index) => {
+  renderHeroProducts () {
+    if (this.props.products.entities.length) {
+      var heroProducts = this.props.products.entities.map((product, index) => {
         return(
           <div key={index} className="item">
             <div className="container padding-top-3x">
@@ -42,35 +42,55 @@ class Home extends Component {
           </div>
         )
       })
-    )
+
+      return (
+        <Carousel id="hero-carousel" nav={true} dots={true} loop={true} navText={["", ""]} items={1} dotsInside={true} largeControls={true} autoplay={true} autoplayTimeOut={7000}>
+          { heroProducts }
+        </Carousel>
+      )
+    }
+  }
+
+  renderFeaturedProducts() {
+    if (this.props.products.entities.length) {
+      var featuredProducts = this.props.products.entities.map((product, index) => {
+        return(
+          <div key={index} className="grid-item">
+            <div className="product-card">
+              <div className="product-badge text-danger"></div><a className="product-thumb" href="shop-single.html"><img src="img/shop/products/09.jpg" alt="Product" /></a>
+              <h3 className="product-title"><a href="shop-single.html">{ product.name }</a></h3>
+              <h4 className="product-price">
+                <del></del>
+              </h4>
+              <div className="product-buttons">
+                <button className="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i className="icon-heart"></i></button>
+                <button className="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        )
+      })
+
+      return (
+        <Carousel id="featured-carousel" dots={true} largeControls={false} dotsInside={false} margin={30} items={1} responsive={ {"0": {"items": 1}, "576": {"items": 2}, "768": {"items": 3}, "991": {"items": 4}, "1200": {"items": 4}} }>
+          { featuredProducts }
+        </Carousel>
+      )
+    }
   }
 
   render() {
     return (
       <div className="home">
         <section className="hero-slider" style={ {backgroundImage: `url(${HeroSliderBg})`} }>
-          <Carousel nav={true} dots={true} loop={true} navText={["", ""]} items={this.props.products.entities.length} autoplay={true} autoplayTimeOut={7000}>
-            { this.renderProductsSlider() }
-          </Carousel>
+          { this.renderHeroProducts() }
         </section>
+
         <section className="container padding-top-3x padding-bottom-3x">
           <h3 className="text-center mb-30">Featured Products</h3>
-          <Carousel nav={true} dots={true} loop={true} navText={["", ""]} items={this.props.products.entities.length} autoplay={true} autoplayTimeOut={7000}>
-            <div className="grid-item">
-              <div className="product-card">
-                <div className="product-badge text-danger">22% Off</div><a className="product-thumb" href="shop-single.html"><img src="img/shop/products/09.jpg" alt="Product" /></a>
-                <h3 className="product-title"><a href="shop-single.html">Rocket Dog</a></h3>
-                <h4 className="product-price">
-                  <del>$44.95</del>$34.99
-                </h4>
-                <div className="product-buttons">
-                  <button className="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i className="icon-heart"></i></button>
-                  <button className="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
-                </div>
-              </div>
-            </div>
-          </Carousel>
+          { this.renderFeaturedProducts() }
         </section>
+
         <section className="container padding-top-3x padding-bottom-2x">
           <div className="row">
             <div className="col-md-3 col-sm-6 text-center mb-30"><img className="d-block w-90 img-thumbnail rounded-circle mx-auto mb-3" src="img/services/01.png" alt="Shipping" />
