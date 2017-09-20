9f0e7c59ed468be161948ce486e6a6346cc38223
Amin Karaji <amin.krj@gmail.com>
2017-09-20 16:31:20 +1000

    changing path after using yarn as a package manager

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'socicon/css/socicon.css'
 
-import 'unishop-template/dist/css/styles.css';
+import 'unishop/dist/css/styles.css';
 import 'feather-icons-sass/feather.css';
 
 import App from './containers/App';
