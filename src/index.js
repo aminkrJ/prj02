5243dea34f48e43d04af330d701592ef8eaf4bf8
Amin Karaji <amin.krj@gmail.com>
2017-09-20 16:30:48 +1000

    Adding social icons

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
+import 'socicon/css/socicon.css'
 
 import 'unishop-template/dist/css/styles.css';
 import 'feather-icons-sass/feather.css';
